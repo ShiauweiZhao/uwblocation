07d7b7f8fd53a937b728ba2ca4dc53f63e78b9a8
zhaoxiaowei <zhaoxiaowei@zerozero.cn>
2018-07-12 10:02:35 +0800

    sstwrresp

--- Src/main.c
+++ Src/main.c
@@ -69,53 +69,61 @@ static void MX_USART1_Init(void);
 /* USER CODE END PFP */
 
 /* USER CODE BEGIN 0 */
-
-/* Default communication configuration. We use here EVK1000's default mode (mode 3). */
+/* Default communication configuration. We use here EVK1000's mode 4. See NOTE 1 below. */
 static dwt_config_t config = {
         2,               /* Channel number. */
         DWT_PRF_64M,     /* Pulse repetition frequency. */
-        DWT_PLEN_1024,   /* Preamble length. Used in TX only. */
-        DWT_PAC32,       /* Preamble acquisition chunk size. Used in RX only. */
+        DWT_PLEN_128,    /* Preamble length. Used in TX only. */
+        DWT_PAC8,        /* Preamble acquisition chunk size. Used in RX only. */
         9,               /* TX preamble code. Used in TX only. */
         9,               /* RX preamble code. Used in RX only. */
-        1,               /* 0 to use standard SFD, 1 to use non-standard SFD. */
-        DWT_BR_110K,     /* Data rate. */
+        0,               /* 0 to use standard SFD, 1 to use non-standard SFD. */
+        DWT_BR_6M8,      /* Data rate. */
         DWT_PHRMODE_STD, /* PHY header mode. */
-        (1025 + 64 - 32) /* SFD timeout (preamble length + 1 + SFD length - PAC size). Used in RX only. */
+        (129 + 8 - 8)    /* SFD timeout (preamble length + 1 + SFD length - PAC size). Used in RX only. */
 };
 
-/* As "TX then wait for a response" example sends a blink message encoded as per the ISO/IEC 24730-62:2013 standard which includes a bit signalling
- * that a response is listened for, this example will respond with a valid frame (that will be ignored anyway) following the same standard. The
- * response is a 21-byte frame composed of the following fields:
- *     - byte 0/1: frame control (0x8C41 to indicate a data frame using 16-bit source addressing and 64-bit destination addressing).
- *     - byte 2: sequence number, incremented for each new frame.
- *     - byte 3/4: application ID (0x609A for data frames in this standard).
- *     - byte 5 -> 12: 64-bit destination address.
- *     - byte 13/14: 16-bit source address, hard coded in this example to keep it simple.
- *     - byte 15: function code (0x10 to indicate this is an activity control message).
- *     - byte 16: activity code (0x00 to indicate activity is finished).
- *     - byte 17/18: new tag blink rate.
- *     - byte 19/20: frame check-sum, automatically set by DW1000.  */
-static uint8 tx_msg[] = {0x41, 0x8C, 0, 0x9A, 0x60, 0, 0, 0, 0, 0, 0, 0, 0, 'D', 'W', 0x10, 0x00, 0, 0, 0, 0};
-/* Indexes to access to sequence number and destination address of the data frame in the tx_msg array. */
-#define DATA_FRAME_SN_IDX 2
-#define DATA_FRAME_DEST_IDX 5
+/* Default antenna delay values for 64 MHz PRF. See NOTE 2 below. */
+#define TX_ANT_DLY 16436
+#define RX_ANT_DLY 16436
 
-/* Inter-frame delay period, in milliseconds. */
-#define TX_DELAY_MS 1000
+/* Frames used in the ranging process. See NOTE 3 below. */
+static uint8 rx_poll_msg[] = {0x41, 0x88, 0, 0xCA, 0xDE, 'W', 'A', 'V', 'E', 0xE0, 0, 0};
+static uint8 tx_resp_msg[] = {0x41, 0x88, 0, 0xCA, 0xDE, 'V', 'E', 'W', 'A', 0xE1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+/* Length of the common part of the message (up to and including the function code, see NOTE 3 below). */
+#define ALL_MSG_COMMON_LEN 10
+/* Index to access some of the fields in the frames involved in the process. */
+#define ALL_MSG_SN_IDX 2
+#define RESP_MSG_POLL_RX_TS_IDX 10
+#define RESP_MSG_RESP_TX_TS_IDX 14
+#define RESP_MSG_TS_LEN 4
+/* Frame sequence number, incremented after each transmission. */
+static uint8 frame_seq_nb = 0;
 
-/* Buffer to store received frame. See NOTE 1 below. */
-#define FRAME_LEN_MAX 127
-static uint8 rx_buffer[FRAME_LEN_MAX];
-/* Index to access to source address of the blink frame in the rx_buffer array. */
-#define BLINK_FRAME_SRC_IDX 2
+/* Buffer to store received messages.
+ * Its size is adjusted to longest frame that this example code is supposed to handle. */
+#define RX_BUF_LEN 12
+static uint8 rx_buffer[RX_BUF_LEN];
 
 /* Hold copy of status register state here for reference so that it can be examined at a debug breakpoint. */
 static uint32 status_reg = 0;
 
-/* Hold copy of frame length of frame received (if good) so that it can be examined at a debug breakpoint. */
-static uint16 frame_len = 0;
+/* UWB microsecond (uus) to device time unit (dtu, around 15.65 ps) conversion factor.
+ * 1 uus = 512 / 499.2 µs and 1 µs = 499.2 * 128 dtu. */
+#define UUS_TO_DWT_TIME 65536
 
+/* Delay between frames, in UWB microseconds. See NOTE 1 below. */
+#define POLL_RX_TO_RESP_TX_DLY_UUS 330
+
+/* Timestamps of frames transmission/reception.
+ * As they are 40-bit wide, we need to define a 64-bit int type to handle them. */
+typedef unsigned long long uint64;
+static uint64 poll_rx_ts;
+static uint64 resp_tx_ts;
+
+/* Declaration of static functions. */
+static uint64 get_rx_timestamp_u64(void);
+static void resp_msg_set_ts(uint8 *ts_field, const uint64 ts);
 
 /* USER CODE END 0 */
 
@@ -164,6 +172,9 @@ int main(void)
     }
     port_set_dw1000_fastrate();
     dwt_configure(&config);
+
+    dwt_setrxantennadelay(RX_ANT_DLY);
+    dwt_settxantennadelay(TX_ANT_DLY);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -174,68 +185,77 @@ int main(void)
   /* USER CODE END WHILE */
 
   /* USER CODE BEGIN 3 */
-        /* Activate reception immediately. See NOTE 4 below. */
         dwt_rxenable(DWT_START_RX_IMMEDIATE);
 
-        /* Poll until a frame is properly received or an error occurs. See NOTE 5 below.
-         * STATUS register is 5 bytes long but, as the events we are looking at are in the lower bytes of the register, we can use this simplest API
-         * function to access it. */
+        /* Poll for reception of a frame or error/timeout. See NOTE 6 below. */
         while (!((status_reg = dwt_read32bitreg(SYS_STATUS_ID)) & (SYS_STATUS_RXFCG | SYS_STATUS_ALL_RX_ERR)))
         { };
 
         if (status_reg & SYS_STATUS_RXFCG)
         {
-            /* A frame has been received, read it into the local buffer. */
-            frame_len = dwt_read32bitreg(RX_FINFO_ID) & RX_FINFO_RXFL_MASK_1023;
-            if (frame_len <= FRAME_LEN_MAX)
-            {
-                dwt_readrxdata(rx_buffer, frame_len, 0);
-            }
-
-            /* TESTING BREAKPOINT LOCATION #1 */
+            uint32 frame_len;
 
             /* Clear good RX frame event in the DW1000 status register. */
             dwt_write32bitreg(SYS_STATUS_ID, SYS_STATUS_RXFCG);
 
-            /* Validate the frame is the one expected as sent by "TX then wait for a response" example. */
-            if ((frame_len == 14) && (rx_buffer[0] == 0xC5) && (rx_buffer[10] == 0x43) && (rx_buffer[11] == 0x2))
+            /* A frame has been received, read it into the local buffer. */
+            frame_len = dwt_read32bitreg(RX_FINFO_ID) & RX_FINFO_RXFL_MASK_1023;
+            if (frame_len <= RX_BUFFER_LEN)
             {
-                int i;
+                dwt_readrxdata(rx_buffer, frame_len, 0);
+            }
 
-                /* Copy source address of blink in response destination address. */
-                for (i = 0; i < 8; i++)
+            /* Check that the frame is a poll sent by "SS TWR initiator" example.
+             * As the sequence number field of the frame is not relevant, it is cleared to simplify the validation of the frame. */
+            rx_buffer[ALL_MSG_SN_IDX] = 0;
+            if (memcmp(rx_buffer, rx_poll_msg, ALL_MSG_COMMON_LEN) == 0)
+            {
+                uint32 resp_tx_time;
+                int ret;
+
+                /* Retrieve poll reception timestamp. */
+                poll_rx_ts = get_rx_timestamp_u64();
+
+                /* Compute final message transmission time. See NOTE 7 below. */
+                resp_tx_time = (poll_rx_ts + (POLL_RX_TO_RESP_TX_DLY_UUS * UUS_TO_DWT_TIME)) >> 8;
+                dwt_setdelayedtrxtime(resp_tx_time);
+
+                /* Response TX timestamp is the transmission time we programmed plus the antenna delay. */
+                resp_tx_ts = (((uint64)(resp_tx_time & 0xFFFFFFFEUL)) << 8) + TX_ANT_DLY;
+
+                /* Write all timestamps in the final message. See NOTE 8 below. */
+                resp_msg_set_ts(&tx_resp_msg[RESP_MSG_POLL_RX_TS_IDX], poll_rx_ts);
+                resp_msg_set_ts(&tx_resp_msg[RESP_MSG_RESP_TX_TS_IDX], resp_tx_ts);
+
+                /* Write and send the response message. See NOTE 9 below. */
+                tx_resp_msg[ALL_MSG_SN_IDX] = frame_seq_nb;
+                dwt_writetxdata(sizeof(tx_resp_msg), tx_resp_msg, 0); /* Zero offset in TX buffer. */
+                dwt_writetxfctrl(sizeof(tx_resp_msg), 0, 1); /* Zero offset in TX buffer, ranging. */
+                ret = dwt_starttx(DWT_START_TX_DELAYED);
+
+                /* If dwt_starttx() returns an error, abandon this ranging exchange and proceed to the next one. See NOTE 10 below. */
+                if (ret == DWT_SUCCESS)
                 {
-                    tx_msg[DATA_FRAME_DEST_IDX + i] = rx_buffer[BLINK_FRAME_SRC_IDX + i];
+                    /* Poll DW1000 until TX frame sent event set. See NOTE 6 below. */
+                    while (!(dwt_read32bitreg(SYS_STATUS_ID) & SYS_STATUS_TXFRS))
+                    { };
+
+                    /* Clear TXFRS event. */
+                    dwt_write32bitreg(SYS_STATUS_ID, SYS_STATUS_TXFRS);
+
+                    /* Increment frame sequence number after transmission of the poll message (modulo 256). */
+                    frame_seq_nb++;
                 }
-
-                /* Write response frame data to DW1000 and prepare transmission. See NOTE 6 below.*/
-                dwt_writetxdata(sizeof(tx_msg), tx_msg, 0); /* Zero offset in TX buffer. */
-                dwt_writetxfctrl(sizeof(tx_msg), 0, 0); /* Zero offset in TX buffer, no ranging. */
-
-                /* Send the response. */
-                dwt_starttx(DWT_START_TX_IMMEDIATE);
-
-                /* Poll DW1000 until TX frame sent event set. */
-                while (!(dwt_read32bitreg(SYS_STATUS_ID) & SYS_STATUS_TXFRS))
-                { };
-
-                /* Clear TX frame sent event. */
-                dwt_write32bitreg(SYS_STATUS_ID, SYS_STATUS_TXFRS);
-
-                /* Increment the data frame sequence number (modulo 256). */
-                tx_msg[DATA_FRAME_SN_IDX]++;
             }
         }
         else
         {
             /* Clear RX error events in the DW1000 status register. */
             dwt_write32bitreg(SYS_STATUS_ID, SYS_STATUS_ALL_RX_ERR);
+
+            /* Reset RX to properly reinitialise LDE operation. */
+            dwt_rxreset();
         }
-
-
-
-
-
 
 
 
@@ -388,17 +408,19 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-/*! ------------------------------------------------------------------------------------------------------------------
- * @fn get_rx_timestamp_u64()
- *
- * @brief Get the RX time-stamp in a 64-bit variable.
- *        /!\ This function assumes that length of time-stamps is 40 bits, for both TX and RX!
- *
- * @param  none
- *
- * @return  64-bit value of the read time-stamp.
- */
-
+static uint64 get_rx_timestamp_u64(void)
+{
+    uint8 ts_tab[5];
+    uint64 ts = 0;
+    int i;
+    dwt_readrxtimestamp(ts_tab);
+    for (i = 4; i >= 0; i--)
+    {
+        ts <<= 8;
+        ts |= ts_tab[i];
+    }
+    return ts;
+}
 
 /*! ------------------------------------------------------------------------------------------------------------------
  * @fn final_msg_set_ts()
@@ -411,6 +433,14 @@ static void MX_GPIO_Init(void)
  *
  * @return none
  */
+static void resp_msg_set_ts(uint8 *ts_field, const uint64 ts)
+{
+    int i;
+    for (i = 0; i < RESP_MSG_TS_LEN; i++)
+    {
+        ts_field[i] = (ts >> (i * 8)) & 0xFF;
+    }
+}
 
 /* USER CODE END 4 */
 
